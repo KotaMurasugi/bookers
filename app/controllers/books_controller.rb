5844b23c0e44bcdbdc9cf9af3c086357160f0377
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-05-14 10:41:04 +0000

    課題2製作中-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -21,9 +21,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
