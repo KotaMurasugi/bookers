c55c6faf225417d0030be25bac0c72ba4c1a0007
KotaMurasugi <132252846+KotaMurasugi@users.noreply.github.com>
2023-05-15 06:09:59 +0000

    課題2途中-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -23,14 +24,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book = Book.find(params[:id])
+      flash[:notice] = "Book was successfully updated."
+      book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   private
